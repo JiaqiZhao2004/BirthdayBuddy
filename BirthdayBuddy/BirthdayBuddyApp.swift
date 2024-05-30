6053d1ebf97d5d6a4f13cea51f206a91b7a6fcfd
Jiaqi Zhao (Roy) <72789180+JiaqiZhao2004@users.noreply.github.com>
2024-05-30 09:09:34 +0800

    Initial Commit

---
+++ BirthdayBuddy/BirthdayBuddyApp.swift
@@ -0,0 +1,17 @@
+//
+//  BirthdayBuddyApp.swift
+//  BirthdayBuddy
+//
+//  Created by Roy Zhao on 5/30/24.
+//
+
+import SwiftUI
+
+@main
+struct BirthdayBuddyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
